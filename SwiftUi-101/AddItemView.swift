1f34751f42913ac002532a37e227bec9416283e9
Nathael007 <nathaelmeunier@gmail.com>
2024-01-23 11:24:06 +0100

    Initial Commit

---
+++ SwiftUi-101/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  SwiftUi-101
+//
+//  Created by Nathael MEUNIER on 23/01/2024.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
---
+++ SwiftUi-101/LootItem.swift
@@ -0,0 +1,18 @@
+//
+//  LootItem.swift
+//  SwiftUi-101
+//
+//  Created by Nathael MEUNIER on 23/01/2024.
+//
+
+import SwiftUI
+
+struct LootItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LootItem()
+}
