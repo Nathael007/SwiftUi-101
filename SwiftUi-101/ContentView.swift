8c853227211b5994f75ff0e372b54ae318833465
Nathael007 <nathaelmeunier@gmail.com>
2024-01-23 10:59:10 +0100

    Initial Commit

---
+++ SwiftUi-101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi-101
+//
+//  Created by Nathael MEUNIER on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi-101/SwiftUi_101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_101App.swift
+//  SwiftUi-101
+//
+//  Created by Nathael MEUNIER on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
