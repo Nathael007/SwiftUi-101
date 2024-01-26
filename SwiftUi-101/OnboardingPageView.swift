fef14796a877e74d432ad1705ecf871e98a8f002
Nathael007 <nathaelmeunier@gmail.com>
2024-01-26 12:01:07 +0100

    tag

---
+++ SwiftUi-101/OnboardingPageView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPageView.swift
+//  SwiftUi-101
+//
+//  Created by Nathael MEUNIER on 26/01/2024.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPageView()
+}
